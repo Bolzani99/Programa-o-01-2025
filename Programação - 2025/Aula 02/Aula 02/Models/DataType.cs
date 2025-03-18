2ebc01045ccceda7772da329fd8b9661d66b5336
rastreamento <rastreamento@RASTREAMENTO>
2025-03-17 21:14:21 -0300

    Aula 02

---
+++ Programação - 2025/Aula 02/Aula 02/Models/DataType.cs
@@ -0,0 +1,47 @@
+﻿namespace Aula_02.Models
+{
+    public class DataType
+    {
+        public char myVar = 'a';
+        public char myConst = 'b';    
+
+        public char myChar1 = 'f';
+        public char myChar2 = ':';
+        public char myChar3 = 'x';
+
+        // podemos também atribuir referencia de caracteres unicode 
+
+        public char myChar4 = '\u2726'; 
+       
+        // podemos ainda mesclar caracteres especiais como, nova linha, tabulação etc.
+        public string textLine = "Esta é uma linha de texto.\n\n\n E esta é outra linha";
+
+        /*
+            \e caracter de escape
+            \n nova linha
+            \r retorno 
+            \e Tabulação horizontal
+            \" aspas duplas, usadas para exibir aspas dentro de string    
+            \' aspas simples, usadas para exibir aspas dentro de string 
+        */
+
+        private int count = 10;
+        public string message;
+        
+        public DataType()
+        {
+            //interpolando strings 
+            // combinando strings de diferentes maneiras no c#
+            message = $"O contador esta em {count}";
+
+            string username = "Silvio Bolzani";
+            int inboxCount = 10;
+            int maxCount = 100;
+
+            message += $"\nO usurario {username}, tem {inboxCount} mensagens.";
+            message += $"\nEspaço restante em sua caixa {maxCount - inboxCount}.";
+        }
+
+
+    }
+}
---
+++ Programação - 2025/Aula 02/Aula 02/Models/TypeCasting.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace Aula_02.Models
+{
+    public class TypeCasting
+    {
+        //Declarando Variáveis na classe 
+        public int myInteger = 20;
+        public long myLong;
+
+        public string myType1;
+        public string myType2;
+
+        public TypeCasting()
+        {
+            //conversão implicita de tipos
+            myLong = myInteger;
+            //myInteger = myLong;
+            //Neste caso o long e muito grande para o int e a
+            //conversão não é permitida
+
+
+            //conversão explicita 
+            long myLong2 = 138129210;
+            int myInteger2;
+            myInteger2 = (int)myLong2;
+
+            // É possivel identificar qual e o tipo
+            // de uma variavel em tempo de execução
+
+            myType1 = myLong2.GetType().ToString();
+            myType2 = myInteger2.GetType().ToString();
+
+        }
+    }
+}
