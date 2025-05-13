ff5227d6201ca893f5fb646081e631c7a6efb020
rastreamento <rastreamento@RASTREAMENTO>
2025-05-12 21:30:28 -0300

    Aula 05

---
+++ Programação - 2025/Aula05/Modelo/Customer.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Modelo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? HomeAddress { get; set; }
+        public string? WorkAddress { get; set; }
+        
+        public bool Validade()
+        {
+            return true;
+        }
+        
+        public Customer Retrieve()
+        {
+            return new Customer();
+        }
+
+        public void Save(Customer customer)
+        {         
+            
+        }
+
+    }
+
+}
---
+++ Programação - 2025/Aula05/Modelo/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public Product? Product { get; set; }
+
+        public double Quantity { get; set; }
+
+        public Double Porchar MyProperty { get; set; }
+    }
+public class OrderItem
+{
+    public int Id { get; set; }
+    public Product? Product { get; set; }
+    public double Quantity { get; set; }
+    public Double Porchar MyProperty { get; set; }
+    }
+}
