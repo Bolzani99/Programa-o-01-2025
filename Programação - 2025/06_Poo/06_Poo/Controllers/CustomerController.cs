9db7391aeecdb3032eac868541e414135c85eea0
rastreamento <rastreamento@RASTREAMENTO>
2025-05-28 16:51:25 -0300

    Atividades

---
+++ Programação - 2025/06_Poo/06_Poo/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _06_Poo.Models;
+using Repository;
+using PooModel;
+
+
+namespace _06_Poo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private CostumerRepository? _customerRepository;
+
+        public CustomerController()
+        {
+            _customerRepository = new CostumerRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Costumer> customers = _customerRepository.RetriveAll();
+            return View(customers);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Costumer c)
+        {
+            _customerRepository.Save(c);
+
+            return View("Index");
+        }
+    }
+}
+
